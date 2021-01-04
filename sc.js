0b52f9ac64b2f8ea6bc54796c8d7236009514a7b
protap <protapdx222@gmail.com>
2021-01-05 01:08:47 +0600

    first commit

---
+++ sc.js
@@ -0,0 +1,17 @@
+var buttons = document.querySelectorAll('button');
+var display = document.querySelector('.result');
+buttons.forEach(function(button) {
+  button.addEventListener('click', calculate);
+});
+function calculate(event) {
+  var clickedButtonValue = event.target.value;
+  if (clickedButtonValue === '=') {
+    if (display.value !== '') { 
+      display.value = eval(display.value);
+    }
+  } else if (clickedButtonValue === 'C') {
+    display.value = '';
+  } else {
+    display.value += clickedButtonValue;
+  }
+}
